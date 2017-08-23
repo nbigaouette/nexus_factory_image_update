0e95d765c8d60c67324d740b2f2d540f7ae480da
Nicolas Bigaouette <nbigaouette@gmail.com>
2017-08-22 21:51:39 -0400

    Move config out to config.sh

---
+++ config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+# AOSP official download:
+# https://developers.google.com/android/images#bullhead
+# https://forum.xda-developers.com/nexus-5x/orig-development/rom-cm14-1-nexus-5x-bullhead-t3496798
+
+download_url="https://dl.google.com/dl/android/aosp"
+
+# 7.1.2 (N2G48C, Aug 2017)
+original_aosp_date="201708"
+original_aosp_version="7.1.2"
+original_aosp_revision="n2g48c"
+original_archive_sha256="45d442a26832292df258d7be83de160f280681fa57a12932a3bccfbf088fc337"
+
+original_archive_filename="bullhead-${original_aosp_revision}-factory-${original_archive_sha256:0:8}.zip"
+
+
+main_dir=`pwd`
+
+extracted_dir="extracted"
+
+final_dir="${main_dir}/aosp_${original_aosp_date}_${original_aosp_version}_${original_aosp_revision}"
--- prepare_files.sh
+++ prepare_files.sh
@@ -2,23 +2,7 @@
 
 set -e
 
-# https://forum.xda-developers.com/nexus-5x/orig-development/rom-cm14-1-nexus-5x-bullhead-t3496798
-# https://developers.google.com/android/images#bullhead
-
-download_url="https://dl.google.com/dl/android/aosp"
-
-# 7.1.2 (N2G48C, Aug 2017)
-original_aosp_date="201708"
-original_aosp_version="7.1.2"
-original_aosp_revision="n2g48c"
-original_archive_sha256="45d442a26832292df258d7be83de160f280681fa57a12932a3bccfbf088fc337"
-
-original_archive_filename="bullhead-${original_aosp_revision}-factory-${original_archive_sha256:0:8}.zip"
-
-extracted_dir="extracted"
-
-
-main_dir=`pwd`
+source config.sh
 
 if [[ ! -f "${original_archive_filename}" ]]; then
     curl -O "${download_url}/${original_archive_filename}"
@@ -30,7 +14,6 @@ echo "${original_archive_sha256}  ${original_archive_filename}" > checksum.sha25
 shasum -a 256 -c checksum.sha256
 
 # Final directory
-final_dir="${main_dir}/aosp_${original_aosp_date}_${original_aosp_version}_${original_aosp_revision}"
 mkdir -p ${final_dir}
 
 # Extract files
